1d9a13f25cc692436f664e9d333925085f7adf28
champo123 <117367934+champo123@users.noreply.github.com>
2023-01-15 22:58:32 +0100

    Add files via upload

--- field.h
+++ field.h
@@ -23,12 +23,12 @@ public:
 
 class Beginning_Field : public Field {
 private:
-    zdzislaw_t bonus;
+    const zdzislaw_t bonus;
 
 public:
     explicit Beginning_Field(const std::string &field_name)
-            : Field(std::move(field_name)) {
-        bonus = 50;
+            : Field(std::move(field_name)),
+              bonus(50) {   
     }
 
     void on_landing(std::shared_ptr<Player> player) override {
@@ -77,7 +77,7 @@ public:
 
 class Goal_Field : public Field {
 private:
-    zdzislaw_t bonus;
+    const zdzislaw_t bonus;
 
 public:
     explicit Goal_Field(const std::string &field_name, zdzislaw_t bonus)
@@ -115,15 +115,15 @@ public:
 
 class Match_Field : public Field {
 private:
-    zdzislaw_t rate;
-    double weight;
+    const zdzislaw_t rate;
+    const double weight;
     zdzislaw_t cumulation;
 public:
     explicit Match_Field(const std::string &field_name, zdzislaw_t rate, double weight)
             : Field(std::move(field_name)),
               rate(rate),
-              weight(weight) {
-        cumulation = 0;
+              weight(weight),
+              cumulation(0) {
     }
 
     void on_landing(std::shared_ptr<Player> player) override {
@@ -140,14 +140,14 @@ public:
 
 class Bookmaker_Field : public Field {
 private:
-    zdzislaw_t rate;
+    const zdzislaw_t rate;
     size_t lucky_element;
 
 public:
     explicit Bookmaker_Field(const std::string &field_name, zdzislaw_t rate)
             : Field(std::move(field_name)),
-              rate(rate) {
-        lucky_element = 0;
+              rate(rate),
+              lucky_element(0) {
     }
 
     void on_landing(std::shared_ptr<Player> player) override {
